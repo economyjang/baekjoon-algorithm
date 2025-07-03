e809181489b5f1c701a51c7438a862519f064b0b
Kyungjae Jang <economyjang777@gmail.com>
2025-07-03 19:02:18 +0900

    [level 2] Title: 올바른 괄호, Time: 7.56 ms, Memory: 9.41 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = True
+    
+    stack = []
+    for char in s:
+        if char == "(":
+            stack.append(char)
+        elif char == ")":
+            if not stack:
+                return False
+            stack.pop()
+
+    return not stack
