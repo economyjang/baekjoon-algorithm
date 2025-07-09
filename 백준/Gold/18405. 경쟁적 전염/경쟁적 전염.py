ee05488219ff171d4b4adfd51ab376b6f593cc0f
Kyungjae Jang <economyjang777@gmail.com>
2025-07-09 17:53:32 +0900

    [Gold V] Title: 경쟁적 전염, Time: 104 ms, Memory: 35708 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -2,35 +2,46 @@ from collections import deque
 
 n, k = map(int, input().split())
 
-graph = []
 data = []
-
-for i in range(n):
-    graph.append(list(map(int, input().split())))
-    for j in range(n):
-        if graph[i][j] != 0:
-            data.append((graph[i][j], 0, i, j))
-
-data.sort()
-q = deque(data)
+for _ in range(n):
+    data.append(list(map(int, input().split())))
 
 target_s, target_x, target_y = map(int, input().split())
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+virus = []
+for x in range(n):
+    for y in range(n):
+        if data[x][y] != 0:
+            virus.append((data[x][y], 0, x, y))
 
-while q:
-    virus, s, x, y = q.popleft()
-    if s == target_s:
-        break
+virus.sort()
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
-        if 0 <= nx and nx < n and 0 <= ny and ny < n:
-            if graph[nx][ny] == 0:
-                graph[nx][ny] = virus
-                q.append((virus, s + 1, nx, ny))
+def bfs():
+    queue = deque(virus)
 
-print(graph[target_x - 1][target_y - 1])
+    while queue:
+        virus_num, time, x, y = queue.popleft()
+
+        if target_s == time:
+            break
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if data[nx][ny] == 0:
+                    data[nx][ny] = virus_num
+                    queue.append((virus_num, time + 1, nx, ny))
+
+bfs()
+
+if data[target_x - 1][target_y - 1] == 0:
+    print(0)
+else:
+    print(data[target_x - 1][target_y - 1])
+
+
