76949b505b5c41aa3361f77a3245a04bf0cb34d0
Kyungjae Jang <economyjang777@gmail.com>
2025-07-10 18:44:31 +0900

    [Silver V] Title: 뒤집기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,18 +1,18 @@
-s = input()
+s = list(input())
 
 count0 = 0
-count1 = 0
+count1 = 0 
 
 if s[0] == '0':
     count0 += 1
-elif s[0] == '1':
+else:
     count1 += 1
 
-for i in range(1, len(s)):
-    if s[i] != s[i - 1]:
-        if s[i] == '0':
+for i in range(len(s) - 1):
+    if s[i] != s[i + 1]:
+        if s[i + 1] == '0':
             count0 += 1
-        elif s[i] == '1':
+        else:
             count1 += 1
 
 print(min(count0, count1))
